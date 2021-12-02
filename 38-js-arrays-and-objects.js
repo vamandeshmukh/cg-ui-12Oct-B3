b29fd55cf238d20cbde01f124793879f886b0076
vamandeshmukh <vamandeshmukh@gmail.com>
2021-12-02 15:56:12 +0530

    Commit

---
+++ 38-js-arrays-and-objects.js
@@ -0,0 +1,31 @@
+// 38-js-arrays-and-objects.js
+
+// const emp = {
+//     eid: 101,
+//     ename: 'Sonu',
+//     salary: 10.5,
+//     address: {
+//         street: 'abc',
+//         pin: 123456
+//     },
+//     phones: [1234, 5678]
+// };
+
+// console.log(emp);
+// console.log(emp.ename);
+// console.log(emp.phones[0]);
+
+const myArray = [10, 20, 'aa', 10.5];
+// console.log(myArray);
+
+// for (let i = 0; i < myArray.length; i++) {
+//     console.log(myArray[i]);
+// }
+
+// for ( int const : myArray) { // Java 
+
+for (const abc of myArray) {
+    console.log(abc);  
+}
+
+
---
+++ 39-js-functions.js
@@ -0,0 +1,36 @@
+
+
+// functions in JS 
+
+// old way , new way 
+
+// old way 
+
+// function fun() {
+//     console.log("fun output");
+// }
+
+// fun();
+
+// new way - arrow function - modern JS 
+//      ->
+
+// const fun2 = () => {
+//     console.log("fun2 output");
+// }
+
+// fun2();
+
+// arguments to functions 
+
+const addNums = (i, j) => {
+    console.log(i + j);
+}
+addNums(10, 20); // 30 
+addNums(10); // NaN 
+addNums(10, 20, 30); // 30 
+addNums(); // NaN
+
+
+
+
