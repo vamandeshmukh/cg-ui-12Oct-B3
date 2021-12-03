a9c4068dfa3d7e6048a809187d8c1f6758c46559
vamandeshmukh <vamandeshmukh@gmail.com>
2021-12-03 15:23:16 +0530

    Update 43-js-callbacks.js

--- 43-js-callbacks.js
+++ 43-js-callbacks.js
@@ -1,16 +1,30 @@
 
+// problems in asynchronous programming 
+// console.log("output 1");
+// const getFun = () => {
+//     setTimeout(() => {
+//         return { message: "fun meesage" }; // react to spring boot 
+//     }, 2000);
+// }
+// // TypeError: Cannot read property 'message' of undefined
+// const fun = getFun();
+// console.log(fun.message); //  depends on output  2
+
+// solution 1. - use callback 
+
 console.log("output 1");
-
-const getFun = () => {
-
+const getFun = (abc) => {
     setTimeout(() => {
-        return { message: "fun meesage" };
+        abc({ message: "fun meesage" }); // callback 
     }, 2000);
 }
 
-const fun = getFun();
+const funCall = (arg) => {
+    console.log(arg.message);
+}
 
-console.log(fun.message); //  depends on output  2
+// success 
+getFun(funCall);
 
 
 
