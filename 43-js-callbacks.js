a095638f1ee3d63cd984d5a78515ac6a15eeb63c
vamandeshmukh <vamandeshmukh@gmail.com>
2021-12-03 15:55:52 +0530

    Update 43-js-callbacks.js

--- 43-js-callbacks.js
+++ 43-js-callbacks.js
@@ -1,5 +1,5 @@
 
-// problems in asynchronous programming 
+// // problems in asynchronous programming 
 // console.log("output 1"); // 1
 // const getFun = () => {
 //     setTimeout(() => {
@@ -11,20 +11,37 @@
 // console.log(fun.message); //  3, fails because it depends on output  2
 
 // solution 1. - use callback 
+// console.log("output 1"); // 1 
 
+// const getFun = (abc) => {  // 3
+//     setTimeout(() => {
+//         abc({ message: "fun meesage" }); // callback  // 4
+//     }, 2000);
+// }
+// const funCall = (arg) => { // 5 
+//     console.log(arg.message);  // 6
+// }
+// // success 
+// getFun(funCall); // 2 
+
+// solution 2 -  use Promise  
 console.log("output 1"); // 1 
 
-const getFun = (abc) => {  // 3
-    setTimeout(() => {
-        abc({ message: "fun meesage" }); // callback  // 4
-    }, 2000);
+const getFun = () => {  // 3
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const err = true; // true / false  
+            if (err)
+                reject();
+            else
+                resolve({ message: "fun meesage" });
+        }, 2000);
+    });
 }
-const funCall = (arg) => { // 5 
-    console.log(arg.message);  // 6
-}
-// success 
-getFun(funCall); // 2 
 
-
-
+getFun().then((obj) => {
+    console.log(obj.message);
+}).catch(() => {
+    console.log("Error...");
+});
 
