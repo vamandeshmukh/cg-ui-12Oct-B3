d051b7f87fcf1e6e92130d5e9c7ab54fdbf210f7
vamandeshmukh <vamandeshmukh@gmail.com>
2021-12-04 21:32:09 +0530

    Commited Login Stuff

--- 50-script.js
+++ 50-script.js
@@ -1,21 +1,18 @@
 
 // 51-login.html 
-const loginFunction = () => {
-
+loginFunction = () => {
     window.localStorage.setItem("user", document.getElementById("userName").value);
-
     if (document.getElementById("userName").value === "Sonu"
         && document.getElementById("password").value === "Sonu") {
         window.location = "./52-welcome.html";
     }
     else {
-        window.location = "./51-login.html";
         console.log("invalid credentials");
     }
 }
 
-
 // 52-welcome.html 
 document.getElementById("welcomeUserName").innerHTML =
     window.localStorage.getItem("user");
+
 
