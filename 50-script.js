392b5f3efa959d491db812a310a63c6499583dd7
vamandeshmukh <vamandeshmukh@gmail.com>
2021-12-04 10:16:10 +0530

    Update 50-script.js

--- 50-script.js
+++ 50-script.js
@@ -2,9 +2,12 @@
 // 51-login.html 
 
 const loginFunction = () => {
+    window.localStorage.setItem("user", document.getElementById("userName").value);
     console.log("loginFunction");
 }
 
 
+document.getElementById("welcomeUserName").innerHTML = 
+window.localStorage.getItem("user");
 
 // 52-welcome.html 
