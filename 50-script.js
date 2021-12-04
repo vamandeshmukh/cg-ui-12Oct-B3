a0e7a38ca581f7d4135896874caf5dea1bc83871
vamandeshmukh <vamandeshmukh@gmail.com>
2021-12-04 10:51:13 +0530

    Commit

--- 50-script.js
+++ 50-script.js
@@ -1,12 +1,15 @@
 
 // 51-login.html 
 const loginFunction = () => {
+
     window.localStorage.setItem("user", document.getElementById("userName").value);
-    if (document.getElementById("userName").value === "vaman"
-        && document.getElementById("password").value === "vaman") {
+
+    if (document.getElementById("userName").value === "Sonu"
+        && document.getElementById("password").value === "Sonu") {
         window.location = "./52-welcome.html";
     }
     else {
+        window.location = "./51-login.html";
         console.log("invalid credentials");
     }
 }
