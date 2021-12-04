59a686e12b41bbbf0f773cc73f0afbdb09bfc22f
vamandeshmukh <vamandeshmukh@gmail.com>
2021-12-04 10:37:41 +0530

    Update 50-script.js

--- 50-script.js
+++ 50-script.js
@@ -1,13 +1,18 @@
 
 // 51-login.html 
-
 const loginFunction = () => {
     window.localStorage.setItem("user", document.getElementById("userName").value);
-    console.log("loginFunction");
+    if (document.getElementById("userName").value === "vaman"
+        && document.getElementById("password").value === "vaman") {
+        window.location = "./52-welcome.html";
+    }
+    else {
+        console.log("invalid credentials");
+    }
 }
 
 
-document.getElementById("welcomeUserName").innerHTML = 
-window.localStorage.getItem("user");
-
 // 52-welcome.html 
+document.getElementById("welcomeUserName").innerHTML =
+    window.localStorage.getItem("user");
+
